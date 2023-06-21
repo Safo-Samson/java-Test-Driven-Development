6791ca09907ce21e5d383aa89f2c58528cd255e0
Liam Radley <liamradley1@gmail.com>
2023-06-21 15:11:20 +0100

    Initial commit

---
+++ src/main/java/com/liamradley/javatddexample/JavaTddExampleApplication.java
@@ -0,0 +1,13 @@
+package com.liamradley.javatddexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaTddExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaTddExampleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/liamradley/javatddexample/JavaTddExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.liamradley.javatddexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaTddExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
